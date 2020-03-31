924e54838d888c46589fadcf23981a37af4208d3
Binh Ho <binh.ho@gmail.com>
2020-03-31 16:43:06 +0700

    First commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+A Simple https run on locally.
+![Screenshot](screenshot.png)
+
+## Get started
+
+```
+$ yarn
+$ node server.js
+```
+
+## Confirm
+
+- https://localhost:4443
+
---
+++ server.js
@@ -0,0 +1,16 @@
+const httpsLocalhost = require("https-localhost")()
+const express = require('express');
+const app = express();
+const path = require('path');
+const https = require('https');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+const port = 4443;
+
+httpsLocalhost.getCerts().then(certs => {
+    https.createServer(certs, app).listen(port, function(){
+        console.log("Server running at https://localhost:" + port)
+    })
+})
